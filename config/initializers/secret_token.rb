218cf23ddae6c4696c29d49ce7345c54e2751e06
Eli Muir <eli.muir@gmail.com>
2011-11-12 12:15:29 -0800

    new app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sw2011Socialweather::Application.config.secret_token = '36f59fa0f0003bda878b49cf87b42bb607478c425058b5f2dfad781ebd2aec5bf4291c3beb68ea6e125255538787995afbb5c8a1be48e45a3cff890437707f5c'
