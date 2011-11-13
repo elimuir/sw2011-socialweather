a0fff01f9c9a7c248951a76f2702607ab00a4a7a
Mohammad Almalkawi <mohammad.almalkawi@gmail.com>
2011-11-13 14:30:35 -0800

    update

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,5 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
-APP_CONFIG = YAML.load(File.open(File.dirname(__FILE__) + "/app_config.yml", "r").read)
-
 # Initialize the rails application
 Sw2011Socialweather::Application.initialize!
